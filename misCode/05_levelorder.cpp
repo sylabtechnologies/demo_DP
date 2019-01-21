049998cabbdc5d362c80a0acac7208570f0f8f13
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2019-01-20 22:06:00 -0500

    Update 05_levelorder.cpp

--- misCode/05_levelorder.cpp
+++ misCode/05_levelorder.cpp
@@ -1,6 +1,7 @@
 // https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
 
-    void levelOrder(Node* root) {
+    void levelOrder(Node* root)
+    {
         queue<Node*> q;
         int levelNodes = 0;
 
@@ -10,20 +11,14 @@
 
         while(!q.empty())
         {
-            levelNodes = q.size();
+            Node* n = q.front();
+            q.pop();
+            cout << n->data << " ";
 
-            while(levelNodes > 0)
-            {
-                Node* n = q.front();
-                q.pop();
-                cout << n->data << " ";
+            if(n->left != nullptr)  q.push(n->left);
 
-                if(n->left != nullptr)  q.push(n->left);
-
-                if(n->right != nullptr) q.push(n->right);
-                
-                levelNodes--;
-            }
+            if(n->right != nullptr) q.push(n->right);
+            
         }
 
         cout << endl;
