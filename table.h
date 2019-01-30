fa7455bb887cb5c9ca3ca55ea00396a32034d583
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2019-01-30 10:48:04 -0500

    init
    init

--- table.h
+++ table.h
@@ -6,14 +6,8 @@ template<typename T> class Table
 public:
     Table(const size_t ysize, const size_t xsize)
     {
-		std::vector<T> row;
-        T empty;
-
-        for (size_t i = 0; i < xsize; i++)
-            row.push_back(empty);
-
-        for (size_t i = 0; i < ysize; i++)
-            _table.push_back(row);
+		std::vector<T> row(xsize);
+        _table = vector<std::vector<T>>(ysize, row);
     }
 
     T& at(const size_t i, const size_t j)
