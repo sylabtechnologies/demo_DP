13179956b03d9b1865c0bb66d5773fd57b98e006
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2019-04-30 16:20:11 -0400

    init
    init

---
+++ miscJava/UnboundedList.java
@@ -0,0 +1,36 @@
+/**
+ * https://docs.oracle.com/javase/tutorial/java/generics/unboundedWildcards.html
+ */
+package objcast;
+import java.util.*;
+
+public class ObjCast {
+    
+    // use unbounded wildcard to virtually use proper function
+    public static void printList(List<?> list)
+    {
+        System.out.print("[");
+        boolean first = true;
+        
+        for (Object elem : list)
+        {
+            if (first)
+                first = false;
+            else
+                System.out.print(", ");
+            
+            System.out.print(elem);
+        }
+
+        System.out.print("]");
+
+    }
+    
+    public static void main(String[] args)
+    {
+        List<String> ls = Arrays.asList("one", "two", "three");
+        printList(ls);
+        System.out.println();
+    }
+    
+}
