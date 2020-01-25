8d18c28eb662ae43ffc51dae0dea6140fa39c69a
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2020-01-25 11:08:50 -0500

    init
    init

---
+++ leetcode/131_RankXform.java
@@ -0,0 +1,53 @@
+package rankxform;
+import java.util.*;
+
+class Solution
+{
+    public static int[] arrayRankTransform(int[] arr)
+    {
+        if (arr == null) return null;
+        if (arr.length == 0) return new int[0];
+        
+        TreeMap<Integer, Integer> rank = new TreeMap<>();
+        
+        for (int i : arr) rank.put(i, 0);
+
+        int curr = 0;
+        for (Map.Entry<Integer, Integer> entry : rank.entrySet())
+        {
+            if (curr == 0)
+            {
+                int key = entry.getKey();
+                entry.setValue(1);
+                curr = 1;
+                continue;
+            }
+            
+            entry.setValue(++curr);
+        }
+        
+        int[] res = new int[arr.length];
+        int count = 0;
+        
+        for (int i : arr)
+        {
+            res[count] = rank.get(i);
+            count++;
+        }
+        
+        return res;
+    }
+}
+
+public class RankXform
+{
+
+    public static void main(String[] args)
+    {
+        int[] arr = {37,12,28,9,100,56,80,5,12};
+
+        System.out.println(Arrays.toString(Solution.arrayRankTransform(arr)));
+        
+    }
+    
+}
