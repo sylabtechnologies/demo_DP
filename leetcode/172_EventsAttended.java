014fdaaf0f241dbae89063f55a6e9ff9274a679b
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2020-02-17 23:56:45 -0500

    init (bfs?)
    init

--- leetcode/172_EventsAttended.java
+++ leetcode/172_EventsAttended.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
-// from Gordon: multimap wont fit all & conflict 3-3
-// clear the logic and redefine as busy intervals
+/// Gordon: multimap by last day, sort by first day
+/// Jesse:  mark attended days w hashset
 
 package eventsattended;
 import java.util.*;
@@ -15,53 +15,39 @@ class Solution
 
         MultiMap map = new MultiMap();
         for (int i = 0; i < len; i++)
-            map.put(events[i][0], events[i][1]);
+            map.put(events[i][1], events[i][0]);
         
         List<Integer> days = map.getKeys(true);
-        int res = 0;
+        HashSet<Integer> busy = new HashSet<>();
 
-//        System.out.println("test " + (++testCase));
-        System.out.println(days);
-        
-        int dayNo = 0;
-        for (Integer d : days)
+        for (Integer lastDay : days)
         {
-            List<Integer> row = map.getRow(d);
+            List<Integer> row = map.getRow(lastDay);
             Collections.sort(row);
-            System.out.println(row);
             
-            boolean startRow = true;
             for (Integer time : row)
             {
-                if (startRow)
-                {
-                    startRow = false;
-                    if (dayNo < d) dayNo = d;
-                }
-
-                if (dayNo <= time)
-                {
-                    res++;
-                    dayNo++;
-                }
+                for (int i = time; i <= lastDay; i++)
+                    if (busy.add(i)) break;
             }
             
         }
 
-        return res;
+        return busy.size();
     }
 }
 
 public class EventsAttended
 {
-
     public static void main(String[] args)
     {
 //        int[][] events = {{1,4},{4,4},{2,2},{3,4},{1,1}};
 //        int[][] events = {{1,1},{1,2},{2,2}};
-
+//        int[][] events = {{1,2},{2,3},{3,4},{1,2}};
+        
         int[][] events = {{1,2},{1,2},{3,3},{1,5},{1,5}};
         Solution obj = new Solution();
         System.out.println(obj.maxEvents(events));
     }
+    
 }
