6cac2df57a1a7022bcd5d668f6e72f43b61aaf83
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2020-03-22 00:07:38 -0400

    init
    init

---
+++ leetcode/210_GivenOrder.java
@@ -0,0 +1,47 @@
+package givenorder;
+import java.util.*;
+
+class Solution
+{
+    public int[] createTargetArray(int[] nums, int[] index)
+    {
+        LinkedList<Integer> target = new LinkedList<>();
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            int ind = index[i];
+            target.add(ind, nums[i]);
+        }
+        
+        System.out.println(target);
+        
+        int[] res = new int[target.size()];
+        int count = 0;
+        for (int i : target)
+            res[count++] = i;
+        
+        prna(res);
+        
+        return res;
+    }
+
+    private void prna(int[] nums)
+    {
+        System.out.println(Arrays.toString(nums));
+    }
+    
+}
+
+public class GivenOrder
+{
+
+    public static void main(String[] args)
+    {
+        Solution sl = new Solution();
+        
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] ind  = {0, 1, 2, 2, 1};
+        sl.createTargetArray(nums, ind);
+    }
+    
+}
