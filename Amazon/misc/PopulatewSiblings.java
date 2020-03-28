7ba6f738532028b3887e61afdae79c024a76c66e
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2020-03-27 20:26:14 -0400

    FIX
    FIX

--- Amazon/misc/PopulatewSiblings.java
+++ Amazon/misc/PopulatewSiblings.java
@@ -3,20 +3,8 @@
 package populatewsiblings;
 
 /// #ama# #D = preorder DFS w/ keeping depth and offset
-
-class Node
-{
-    int val;
-    Node left;
-    Node right;
-    Node next;
-    Node(int x) { val = x; }
-    void addLeft(int x) { left = new Node(x); }
-    void addRight(int x) { right = new Node(x); }
-
-    @Override
-    public String toString() { return "[ " + val + ", " + left + ", " + right + " ]"; }
-}
+/// #DD -> model at once
+/// fin = link subtrees w/ right dfs = left dfs
 
 class Solution
 {
@@ -24,20 +12,19 @@ class Solution
     {
         if (root == null) return null;
 
-        helper(root);
+        preorder(root);
         return root;
     }
 
-    private Node helper(Node root)
+    private Node preorder(Node root)
     {
-        if (root.left == null) return null;
-        
         System.out.println(" Node " + root.val + ":");
-        
-        // if one is checked the other is fine
-        /// make a chain
-        root.left.next = helper(root.right);
-        helper(root.left);
+
+        if (root.left != null)
+        {
+            preorder(root.left);
+            root.left.next = preorder(root.right);
+        }
         
         return root;        
     }
@@ -61,7 +48,7 @@ public class PopulatewSiblings
         Solution s = new Solution();
         s.connect(root);
         
-       // printConnections(root);
+        printConnections(root);
     }
 
     private static void printConnections(Node root)
