e505b35557670eb1d27690cef283e8445a1d5b87
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2020-05-17 15:14:54 -0400

    init
    init

--- leetcode/295_ExcludeFavsSubsets.java
+++ leetcode/295_ExcludeFavsSubsets.java
@@ -3,6 +3,71 @@
 package favcompanysubset;
 import java.util.*;
 
+class Elem implements Comparable<Elem>
+{
+    public final int index;
+    private int data[];
+    private int dataLen;
+
+    public Elem(List<String> row, HashMap<String, Integer> index, int i)
+    {
+        this.index = i;
+        
+        TreeSet<Integer> set = new TreeSet<>();
+        for (String word : row)
+            set.add(index.get(word));
+        
+        dataLen = set.size();
+        data = new int[dataLen];
+        
+        int cnt = 0;
+        for (Integer n : set)
+            data[cnt++] = n;
+    }
+
+    public int size() {       return dataLen; }
+    
+    boolean isSubsetOf(Elem el)
+    {
+        if (this.dataLen >= el.dataLen) return false;
+        
+        int i = 0, j = 0;
+        while (i < this.dataLen && j < el.dataLen)
+        {
+            int el1 = this.data[i];
+            int el2 = el.data[j];
+            
+            if (el1 == el2)
+            {
+                i++; j++;
+            }
+            else if (el1 > el2)
+                j++;
+            else
+                return false;
+        }
+        
+        return i == this.dataLen;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "[" + index + " : " + Arrays.toString(data) + "]";
+    }
+
+    @Override
+    public int compareTo(Elem s2)
+    {
+        if (this.size() < s2.size())
+            return -1;
+        else if (this.size() > s2.size())
+            return 1;
+        else
+            return 0;
+    }
+}
+
 class Solution
 {
     public List<Integer> peopleIndexes(List<List<String>> favs)
@@ -21,73 +86,31 @@ class Solution
         ArrayList<Elem> otherFavs = new ArrayList<>();
         count = 0;
         for (List<String> row : favs)
-        {
-            TreeSet<Integer> other = new TreeSet<>();
-            for (String word : row)
-                other.add(index.get(word));
-            
-            otherFavs.add(new Elem(other, count++));
-        }
-        
-        Collections.sort(otherFavs, new Compare());
-//        System.out.println(otherFavs);
+            otherFavs.add(new Elem(row, index, count++));
+        Collections.sort(otherFavs);
         
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < otherFavs.size(); i++)
         {
-            Elem row = otherFavs.get(i);
-            int currSize = row.set.size();
-            
             boolean isSubset = false;
+            Elem set1 = otherFavs.get(i);
+            
             for (int j = i + 1; j < otherFavs.size(); j++)
             {
                 if (j == i) continue;
                 
-                TreeSet<Integer> cmp = otherFavs.get(j).set;
-                if (cmp.size() == currSize) continue;
-                
-                isSubset = cmp.containsAll(row.set);
-
-                if (isSubset) break;
+                Elem set2 = otherFavs.get(j);
+                if (set1.isSubsetOf(set2))
+                {
+                    isSubset = true; break;
+                }
             }
             
-            if (!isSubset) ans.add(row.index);
+            if (!isSubset) ans.add(set1.index);
         }
         
         Collections.sort(ans);
         return ans;
-    }
-
-    private static class Elem
-    {
-        TreeSet<Integer> set;
-        int index;
-        
-        private Elem(TreeSet<Integer> other, int ind)
-        {
-            this.set = new TreeSet<>(other);
-            this.index = ind;
-        }
-
-        @Override
-        public String toString()
-        {
-            return "[" + index + " : " + set.toString() + "]";
-        }
-    }
-
-    private static class Compare implements Comparator<Elem>
-    {
-        @Override
-        public int compare(Elem s1, Elem s2)
-        {
-            if (s1.set.size() < s2.set.size())
-                return -1;
-            else if (s1.set.size() > s2.set.size())
-                return 1;
-            else
-                return 0;
-        }
     }
 }
 
