88fe9e492adf4187df0618959c3de394f4eac40f
sylabtechnologies <teslacloud@icloud.com>
2020-10-02 16:29:07 -0400

    ini + limit

---
+++ Google/007_NiteMoves.java
@@ -0,0 +1,112 @@
+// https://leetcode.com/problems/minimum-knight-moves/
+
+package goog2;
+import java.util.*;
+
+class Solution
+{
+    private final static int moves[][] = 
+    { {1,2}, {2,1},  {2,-1}, {1,-2},
+      {-1,-2}, {-2,-1}, {-2,1}, {-1,2} };
+    
+    public int minKnightMoves(int finx, int finy)
+    {
+        Queue<Point> bfs = new LinkedList<>();
+        bfs.add(new Point(0, 0, 0));
+        HashSet<Point> visited = new HashSet<>();
+        
+        finx = Math.abs(finx);
+        finy = Math.abs(finy);
+        
+        int xmin = -2, xmax = finx + 2,
+            ymin = -2, ymax = finy + 2;
+        
+        while (!bfs.isEmpty())
+        {
+            Point pt = bfs.poll();
+            if (pt.x == finx && pt.y == finy) return pt.time;
+
+            if (visited.contains(pt))
+                continue;
+            else
+                visited.add(pt);
+
+            for (int[] move : moves)
+            {
+                if ( pt.x + move[0] < xmin || pt.x + move[0] > xmax) continue;
+                if ( pt.y + move[1] < ymin || pt.y + move[1] > ymax) continue;
+                
+                Point next = new Point(pt.x + move[0], pt.y + move[1], pt.time + 1);
+                bfs.add(next);
+            }
+        }
+
+        return -1;
+    }
+
+    private static class Point
+    {
+        int x, y, time;
+        public Point(int x, int y, int t) {this.x = x;this.y = y; time = t;}
+
+        @Override
+        public String toString() { return "(" + x + ", " + y + ')';}
+        
+        @Override
+        public int hashCode()
+        {
+            int hash = 7;
+            hash = 47 * hash + this.x;
+            hash = 47 * hash + this.y;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Point other = (Point) obj;
+            
+            if (this.x == other.x && this.y == other.y)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
+public class Goog2
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Solution().minKnightMoves(2, 1));
+    }
+    
+}
+
+class Node
+{
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
