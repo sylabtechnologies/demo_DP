bc8f4ba7c66b4ac187b436be27bf06e3c228a041
sylabtechnologies <teslacloud@icloud.com>
2020-10-05 16:18:17 -0400

    A or B?

--- Google/013_NonDecreasArr.java
+++ Google/013_NonDecreasArr.java
@@ -7,58 +7,48 @@ class Solution
 {
     public boolean checkPossibility(int[] nums)
     {
-        int hit = findNondecreasing(nums, -1);
+        int hit = findNondecr(nums, -1);
         if (hit < 0) return true;
+
+        int res1 = findNondecr(nums, hit);
+        if (res1 < 0) return true;
+
+        int res2 = findNondecr(nums, hit-1);
+        if (res2 < 0) return true;
         
-        LinkedList<Integer> lst1 = fillnskip(nums, hit);
-        LinkedList<Integer> lst2 = fillnskip(nums, hit-1);
-        
-        return nonDecresase(lst1) || nonDecresase(lst2);
+        return false;
     }
 
-    private int findNondecreasing(int arr[], int skip)
+    private int findNondecr(int arr[], int skip)
     {
-        int hit = -1;
-        for (int i = 1; i < arr.length; i++)
+        int i = 0, j = 1;
+        
+        if (skip == 0)
         {
-            if (i == skip) continue;
-            
-            if (arr[i-1] > arr[i])
+            i++; j++;            
+        }
+        
+        boolean skipped = false;
+        while (j < arr.length)
+        {
+            if (j == skip)
             {
-                hit = i; break;
+                skipped = true;
+                j++;
+                continue;
             }
+            
+            if (arr[i] > arr[j]) return j;
+            
+            if (skipped)
+            {
+                skipped = false;
+                i++;
+            }
+            
+            i++; j++;            
         }
 
-        return hit;
-    }
-
-    private LinkedList<Integer> fillnskip(int[] nums, int skip)
-    {
-        LinkedList<Integer> ret = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++)
-        {
-            if (i == skip) continue;
-            ret.add(nums[i]);            
-        }
-        
-        return ret;
-    }
-
-    private boolean nonDecresase(LinkedList<Integer> lst)
-    {
-        for (int i = 1; i < lst.size(); i++)
-            if (lst.get(i-1) > lst.get(i)) return false;
-        
-        return true;
-    }
-}
-
-public class Goog7
-{
-    public static void main(String[] args)
-    {
-        System.out.println(new Solution().checkPossibility(new int[]{4,2,3}));
-        System.out.println(new Solution().checkPossibility(new int[]{1,2,1,3,4}));
-        System.out.println(new Solution().checkPossibility(new int[]{5,7,1,8}));
+        return -1;
     }
 }
