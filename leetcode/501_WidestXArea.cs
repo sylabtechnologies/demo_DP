79301d67e9ea0f73538f8050d8dad805a48d3f91
sylabtechnologies <teslacloud@icloud.com>
2020-11-01 15:13:48 -0500

    ini

---
+++ leetcode/501_WidestXArea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+
+namespace WidestArea
+{
+    public class Solution
+    {
+        public int MaxWidthOfVerticalArea(int[][] points)
+        {
+            SortedSet<int> xpts = new SortedSet<int>();
+            foreach (var pt in points)
+                xpts.Add(pt[0]);
+
+            int prev = xpts.First();
+            int max = Int32.MinValue;
+            foreach (var pt in xpts)
+            {
+                int dist = pt - prev;
+                max = Math.Max(max, dist);
+                prev = pt;
+            }
+
+            return max;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
