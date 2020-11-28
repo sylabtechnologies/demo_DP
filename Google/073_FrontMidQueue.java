c0f33034e11ee0175857eb286d6b9eda0b11ddf0
sylabtechnologies <teslacloud@icloud.com>
2020-11-28 13:54:24 -0500

    ini

--- Google/073_FrontMidQueue.java
+++ Google/073_FrontMidQueue.java
@@ -1,89 +1,77 @@
 package goog25;
 import java.util.*;
 
-public class Goog25
+// Design a queue that supports push and pop operations in the front, middle, and back
+// front == back || back - 1
+class FrontMiddleBackQueue
 {
-    public static void main(String[] args)
+    private LinkedList<Integer> front;
+    private LinkedList<Integer> back;
+
+    public FrontMiddleBackQueue()
     {
-        
+        front = new LinkedList<>();
+        back  = new LinkedList<>();
     }
-}
-
-/*
-//        int arr[]= {6, 1, 7, 2, 1 };
-//        System.out.println(new Solution().waysToMakeFair(arr));
-
-class Solution
-{
-    public int waysToMakeFair(int[] nums)
+    
+    public void pushFront(int val)
     {
-        final int mylen = nums.length;
-        if (mylen < 3) return 0;
-        
-        int sums[] = new int[mylen];
-        sums[0] = nums[0];
-        sums[1] = nums[1];
-        
-        for (int i = 2; i < sums.length; i += 2)
-        {
-            sums[i] = sums[i-2] + nums[i];
-            
-            if (i < sums.length - 1)
-                sums[i+1] = sums[i-1] + nums[i+1];
-        }
-        
-        int lastEven =0, lastOdd = 0;
-        if (mylen % 2 == 0)
-        {
-            lastEven = sums[mylen-2];
-            lastOdd = sums[mylen-1];
-        }
+        front.addFirst(val);
+
+        if (front.size() > back.size())
+            back.addFirst(front.removeLast());
+    }
+    
+    public void pushMiddle(int val)
+    {
+        if (front.size() == back.size())
+            back.addFirst(val);
         else
-        {
-            lastEven = sums[mylen-1];
-            lastOdd = sums[mylen-2];
-        }
-/*
-     [6, 1, 7, 2, 1] fair to remove #1
-      6     13    14
-         1      3
+            front.addLast(val);
         
-      6  1  2  1    re 7
+        System.out.println(front + " : " + back);
+    }
+    
+    public void pushBack(int val)
+    {
+        if (front.size() == back.size() - 1)
+            front.addLast(back.removeFirst());
+
+        back.addLast(val);
+    }
+    
+    public int popFront()
+    {
+        if (front.isEmpty())
+            return back.isEmpty() ? -1 : back.removeFirst();
+
+        if (back.size() > front.size())
+            front.addLast(back.removeFirst());
         
-      6     8  
-         1      2
+        return front.removeFirst();
+    }
+    
+    public int popBack()
+    {
+        if (front.isEmpty() && back.isEmpty())
+            return -1;
         
+        if (front.size() == back.size())
+            back.addFirst(front.removeLast());
         
-        int count = 0;
-        for (int i = 0; i < sums.length; i++)
-        {
-            int odd = 0, even = 0;
-            
-            int remove = nums[i];
-                        
-            if (i % 2 == 0)
-            {
-                even = lastOdd - sums[i];
-                odd = sums[i] + lastOdd - remove;
-            }
-            else
-            {
-                even = sums[i-1] + lastOdd - remove;
-                odd = lastEven - sums[i-1];
-            }
-            
-            if (odd == even) count++;
-                    
-        }
-     
-        return count;
+        return back.removeLast();
+    }
+    
+    public int popMiddle()
+    {
+        if (back.isEmpty() && front.isEmpty())
+            return -1;
+
+        if (front.size() == back.size())
+            return front.removeLast();
+
+        return back.removeFirst();
     }
 }
 
 
-    remove 1,
-    6 7 2 1 is fair
-    6   8    plus even
-      7   8  minus even
-
-*/
