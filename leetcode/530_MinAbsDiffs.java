1c6f9e2c04940b9a58b6c72452f051887f0e1428
sylabtechnologies <teslacloud@icloud.com>
2020-12-13 21:53:55 -0500

    ini

--- leetcode/530_MinAbsDiffs.java
+++ leetcode/530_MinAbsDiffs.java
@@ -1,5 +1,5 @@
 package goog27;
-import java.util.*;
+import java.util.Arrays;
 
 // https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
 class Solution
@@ -7,25 +7,22 @@ class Solution
     public int[] getSumAbsoluteDifferences(int[] nums)
     {
         final int len = nums.length;
-        int prefixSum[]  = new int[len];
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < len; i++)
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-            
+        PrefixSum pfs = new PrefixSum(nums);
+        
         int ans[] = new int[len];
         for (int i = 1; i < nums.length-1; i++)
         {
-            int rgt = prefixSum[len-1] - prefixSum[i];
+            int rgt = pfs.getSum(i+1, len-1);
             rgt -= nums[i]*(len-1-i);
             
-            int lft = nums[i]*(i+1);
-            lft -= prefixSum[i];
+            int lft = nums[i]*i;
+            lft -= pfs.getSum(0, i-1);
 
             ans[i] = rgt + lft;
         }
         
-        ans[0] = prefixSum[len-1] - nums[0]*len;
-        ans[len-1] = nums[len-1]*(len-1) - prefixSum[len-2];
+        ans[0] = pfs.getSum(0, len-1) - nums[0]*len;
+        ans[len-1] = nums[len-1]*(len-1) - pfs.getSum(0, len-2);
         return ans;
     }
 }
